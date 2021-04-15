caa292d6188f8290863c7fda067aa9bb16dfe55d
vincehex <1648146511@qq.com>
2021-04-15 13:05:55 +0800

    Default Changelist

---
+++ Display/views.py
@@ -0,0 +1,16 @@
+from django.http import request
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def contact(request):
+    return render(request, 'contact.html')
